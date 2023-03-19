f482980dee745cfcb74507aca9b021beb7f51a33
Tomatcree01 <76787776+Tomatcree01@users.noreply.github.com>
2023-03-19 14:54:29 -0400

    Add files via upload

--- stuff/js/cursor.js
+++ stuff/js/cursor.js
@@ -4,6 +4,7 @@ const { gsap, CircleType } = window;
 
 const cursorOuter = document.querySelector(".cursor--large");
 const cursorInner = document.querySelector(".cursor--small");
+const clock = document.querySelector(".clock");
 const cursorTextContainerEl = document.querySelector(".cursor--text");
 const cursorTextEl = cursorTextContainerEl.querySelector(".text");
 
@@ -34,34 +35,56 @@ let mouse = {
 
 
 //double click and single click
-	document.body.addEventListener("mousedown", (e) => {
-	if(e.detail === 2) { // check if it was a double-click event or single-click event
-	   gsap.to(cursorInner, hoverEffectDuration, {
-		 scale: 10.4   // set inner cursor scale to 0.4
-	   });
-	   gsap.to(cursorOuter, hoverEffectDuration, {
-		 scale: 0.3    // set outer cursor scale to 1
-	   });
+document.body.addEventListener("mousedown", (e) => {
+	if (e.detail === 2) { // check if it was a double-click event or single-click event
+		gsap.to(cursorInner, hoverEffectDuration, {
+			scale: 10.4   // set inner cursor scale to 0.4
+		});
+		gsap.to(cursorOuter, hoverEffectDuration, {
+			scale: 0.3    // set outer cursor scale to 1
+		});
+		let rotateInterval;
+
+		// Add event listeners for when the user presses and releases a button
+		document.addEventListener('mousedown', function (event) {
+			if (event.code === 'click') { // replace 'Space' with the desired button key code
+				rotateInterval = setInterval(rotateElement, 10); // replace 10 with the desired rotation speed
+			}
+		});
+
+		document.addEventListener('mouseup', function (event) {
+			if (event.code === 'click') {
+				clearInterval(rotateInterval); // stop rotating the element
+			}
+		});
+
+		// This function will continually rotate the specified element
+		function rotateElement() {
+			const element = document.querySelector(".clock");
+
+			// Modify the style of the element to make it rotate. You can adjust the transform values for different rotation directions and speeds
+			element.style.transform += 'rotate(20deg)'; // replace 1deg with the desired amount of rotation per interval
+		}
 	} else { // otherwise, execute single click event
-	   gsap.to(cursorInner, hoverEffectDuration, {
-		scale: 3.5
-	   });
-	   gsap.to(cursorOuter, hoverEffectDuration, { 
-		 scale: 0.4 
-	   });
+		gsap.to(cursorInner, hoverEffectDuration, {
+			scale: 3.5
+		});
+		gsap.to(cursorOuter, hoverEffectDuration, {
+			scale: 0.4
+		});
 	}
-  });
-  
-  document.body.addEventListener("mouseup", (e) => {
-	gsap.to(cursorInner, hoverEffectDuration, { 
-	  scale: 1 
+});
+
+document.body.addEventListener("mouseup", (e) => {
+	gsap.to(cursorInner, hoverEffectDuration, {
+		scale: 1
 	});
-	gsap.to(cursorOuter, hoverEffectDuration, { 
-	  scale: 1.2
+	gsap.to(cursorOuter, hoverEffectDuration, {
+		scale: 1.2
 	});
-  });
-  
-  
+});
+
+
 
 document.body.addEventListener("pointermove", updateCursorPosition);
 
@@ -143,10 +166,10 @@ function handlePointerEnter(e) {
 	});
 }
 
-  
+
 hoverItems.forEach((item) => {
-item.addEventListener("click", handleClick);
-item.addEventListener("pointerleave", handlePointerLeave);
+	item.addEventListener("click", handleClick);
+	item.addEventListener("pointerleave", handlePointerLeave);
 });
 
 function handlePointerLeave() {
